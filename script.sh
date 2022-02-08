5770cab29aa888148385b60972564b1e49398701
sasideepika <69594878+sasideepika@users.noreply.github.com>
2022-02-08 19:15:21 +0530

    Update and rename README.md to script.sh

---
+++ script.sh
@@ -0,0 +1,17 @@
+##Install Jfrog
+
+wget -qO - https://releases.jfrog.io/artifactory/jfrog-gpg-public/jfrog_public_gpg.key | sudo apt-key add -
+
+echo "deb https://releases.jfrog.io/artifactory/jfrog-debs xenial contrib" | sudo tee -a /etc/apt/sources.list;
+
+apt update;
+
+apt install -y jfrog-cli-v2-jf;
+
+
+##Install Npm
+
+
+sudo apt update -y
+sudo apt install nodejs -y
+sudo apt install npm -y
